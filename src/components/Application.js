2d91297fad4a4a1041b3d9311b22fa42b972d790
Nicholas <nc_chau@hotmail.com>
2022-10-06 15:42:32 -0400

    replace days data with axios request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
-const days = [
-  {
+const appointments = {
+  "1": {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
-  {
+  "2": {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
-  {
+  "3": {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
-];
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] =useState([]);
+  const appointmentsArray = Object.values(appointments).map((appointment) => {
+      return (
+        <Appointment 
+          key={appointment.id} 
+          {...appointment} 
+        />
+      )
+  });
+
+  useEffect(() => {
+    axios
+    .get("/api/days")
+    .then(response => {
+      console.log("retrieved!");
+      setDays(response.data);
+    })
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -47,7 +89,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentsArray}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
